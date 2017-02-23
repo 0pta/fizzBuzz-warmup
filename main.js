314fa316efea004290eb813ac0014b7e05a8620a
DerekWW <derek.wierson@gmail.com>
2017-02-23 08:24:32 -0800

    work on warmup

---
+++ main.js
@@ -0,0 +1,18 @@
+// Write a function called fizzBuzz that will do the following:
+// 1) Go through the numbers 1-100
+// 2) If the number is a multiple of 3 print fizz
+// 3) If the number is a multiple of 5 print buzz
+// 4) If the number is a multiple of 3 and 5 print fizzBuzz
+// 5) If the number doesnt match any of the above then simply print the number
+
+
+
+// ***Bonus***
+// Write a function called fizzBuzz2 that does the above, but instead of hardcoded
+// numbers pass them as parameters. ex: fizzBuzz2(500, 2, 9)
+
+
+
+// ***Super Bonus***
+// Write a function called fizzBuzz3 that does the same as the bonus above, but recursivly.
+// Hint: you might need a counter variable starting at 1!
